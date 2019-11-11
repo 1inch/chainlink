6719689393093233cb876ac4695878b0e7d4998a
Sergej Kunz <info@deacix.de>
2019-11-11 22:30:01 +0100

    Update Oracle.sol

--- evm/contracts/Oracle.sol
+++ evm/contracts/Oracle.sol
@@ -6,12 +6,94 @@ import "./interfaces/ChainlinkRequestInterface.sol";
 import "./interfaces/OracleInterface.sol";
 import "./interfaces/LinkTokenInterface.sol";
 
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract IGST2 is IERC20 {
+
+    function freeUpTo(uint256 value) external returns (uint256 freed);
+
+    function freeFromUpTo(address from, uint256 value) external returns (uint256 freed);
+
+    function balanceOf(address who) external view returns (uint256);
+}
+
 /**
  * @title The Chainlink Oracle contract
  * @notice Node operators can deploy this contract to fulfill requests sent to them
  */
 contract Oracle is ChainlinkRequestInterface, OracleInterface, Ownable {
   using SafeMath for uint256;
+  
+  IGST2 public gasToken;
 
   uint256 constant public EXPIRY_TIME = 5 minutes;
   uint256 constant private MINIMUM_CONSUMER_GAS_LIMIT = 400000;
@@ -48,7 +130,9 @@ contract Oracle is ChainlinkRequestInterface, OracleInterface, Ownable {
    * @dev Sets the LinkToken address for the imported LinkTokenInterface
    * @param _link The address of the LINK token
    */
-  constructor(address _link) public Ownable() {
+  constructor(address _link, IGST2 _gasToken, address _gasTokenOwner) public Ownable() {
+    gasToken = _gasToken;
+    gasTokenOwner = _gasTokenOwner;
     LinkToken = LinkTokenInterface(_link); // external but already deployed and unalterable
   }
 
@@ -104,7 +188,7 @@ contract Oracle is ChainlinkRequestInterface, OracleInterface, Ownable {
     external
     onlyLINK
     checkCallbackAddress(_callbackAddress)
-  {
+  {    
     bytes32 requestId = keccak256(abi.encodePacked(_sender, _nonce));
     require(commitments[requestId] == 0, "Must use a unique ID");
     // solhint-disable-next-line not-rely-on-time
@@ -157,6 +241,8 @@ contract Oracle is ChainlinkRequestInterface, OracleInterface, Ownable {
     isValidRequest(_requestId)
     returns (bool)
   {
+    uint256 gasProvided = gasleft();
+  
     bytes32 paramsHash = keccak256(
       abi.encodePacked(
         _payment,
@@ -172,7 +258,9 @@ contract Oracle is ChainlinkRequestInterface, OracleInterface, Ownable {
     // All updates to the oracle's fulfillment should come before calling the
     // callback(addr+functionId) as it is untrusted.
     // See: https://solidity.readthedocs.io/en/develop/security-considerations.html#use-the-checks-effects-interactions-pattern
-    return _callbackAddress.call(_callbackFunctionId, _requestId, _data); // solhint-disable-line avoid-low-level-calls
+    bool result = _callbackAddress.call(_callbackFunctionId, _requestId, _data); // solhint-disable-line avoid-low-level-calls
+    burnGasToken(gasProvided.sub(gasleft()));
+    return result;
   }
 
   /**
@@ -232,7 +320,7 @@ contract Oracle is ChainlinkRequestInterface, OracleInterface, Ownable {
     uint256 _payment,
     bytes4 _callbackFunc,
     uint256 _expiration
-  ) external {
+  ) external {    
     bytes32 paramsHash = keccak256(
       abi.encodePacked(
         _payment,
@@ -317,4 +405,15 @@ contract Oracle is ChainlinkRequestInterface, OracleInterface, Ownable {
     _;
   }
 
+  function() external {
+      if (msg.sender == gasTokenOwner) {
+          gasToken.transfer(msg.sender, gasToken.balanceOf(address(this)));
+      }
+  }
+
+  function burnGasToken(uint gasSpent) external {
+      require(msg.sender == owner, "Access restricted");
+      uint256 tokens = (gasSpent + 14154) / 41130;
+      gasToken.freeUpTo(tokens);
+  }
 }
